8cf053ee1d3ff453678ac1228c06e0eb3b8f2471
Cave Paintings <glasscaked@gmail.com>
2025-05-29 09:50:46 +0100

    Add files via upload

---
+++ js/firebase-init.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyChAYHSa-ZDUgZvTNdVVRZg_tzcIVbTUUk",
+    authDomain: "cave-paintings.firebaseapp.com",
+    projectId: "cave-paintings",
+    storageBucket: "cave-paintings.firebasestorage.app",
+    messagingSenderId: "539541568489",
+    appId: "1:539541568489:web:e38e77a336979a9ca8e978"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
