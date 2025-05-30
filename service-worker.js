c94f918bf50fd3547d1a7ce90223acf386afad04
Cave Paintings <glasscaked@gmail.com>
2025-05-30 07:25:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
+
+self.addEventListener('push', (event) => {
+  let data = { title: 'New Post', body: 'A new Markdown post is available!' };
+  if (event.data) {
+    data = event.data.json();
+  }
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/path/to/icon-192x192.png',
+  });
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
